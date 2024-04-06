75d4cbfe7637483997556ca75ab7ef18b583615c
Yumeng Liu <yumel13@uci.edu>
2024-04-06 10:04:07 -0700

    Added test cases

--- test_augmented_matrix.py
+++ test_augmented_matrix.py
@@ -12,9 +12,32 @@ class TestAugmentedMatrix(unittest.TestCase):
         )
         expected_solution = [1, 0, -1]
         solution = self.augmented_matrix.solve()
-        print('Actual:\n', solution)
-        print('Expected:\n', expected_solution)
         self.assertTrue(expected_solution, solution)
+
+    def test_augmented_matrix2(self):
+        self.augmented_matrix = AugmentedMatrix(
+            [[1, -3, 0, 5],
+             [-1, 1, 5, 2],
+             [0, 1, 1, 0]]
+        )
+        expected_solution = [2, -1, 1]
+
+    def test_augmented_matrix_empty(self):
+        self.augmented_matrix = AugmentedMatrix(
+            [[0, 0, 0],
+             [0, 0, 0]]
+        )
+        with self.assertRaises(ValueError):
+            self.augmented_matrix.solve()
+
+    def test_no_solution(self):
+        self.augmented_matrix = AugmentedMatrix(
+            [[1, 2, 3, 4],
+             [1, 2, 3, 5],
+             [3, 0, 4, 2]]
+        )
+        with self.assertRaises(ValueError):
+            self.augmented_matrix.solve()
 
 
 if __name__ == '__main__':
