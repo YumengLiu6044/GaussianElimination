325fe489b3eedbeb85b3cdc9c1a678463813b4a1
Yumeng Liu <yumel13@uci.edu>
2024-04-15 14:47:58 -0700

    Added a function that checks if the matrix is in echelon form

--- augmented_matrix/augmented_matrix.py
+++ augmented_matrix/augmented_matrix.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+NON_SIMPLIFIED = -1
+UPPER_TRIANGULAR = 0
+REDUCED_ECHELON = 1
+
 
 class NoSolutionError(Exception):
     pass
@@ -7,7 +11,8 @@ class NoSolutionError(Exception):
 
 class AugmentedMatrix(np.ndarray):
     def __new__(cls, *args, **kwargs):
-        return np.asarray(*args, dtype=np.float64, **kwargs).view(cls)
+
+        return np.asarray(*args, **kwargs).view(cls)
 
     def _check_valid_solution(self):
         """
@@ -25,6 +30,7 @@ class AugmentedMatrix(np.ndarray):
         """
         row, column = 0, 0
         while row < (row_count := self.shape[0]) and column < self.shape[1]:
+            # Use the highest absolute value as pivot
             max_row_index = row
             for i in range(row, self.shape[0]):
                 if abs(self[i][column]) > abs(self[max_row_index][column]):
@@ -44,8 +50,6 @@ class AugmentedMatrix(np.ndarray):
             column += 1
             row = column
 
-        self._state = 'upper_triangular'
-
     def _simplify_echelon(self):
         """
         Simplifies the upper triangular form so that the pivots are 1
@@ -60,16 +64,23 @@ class AugmentedMatrix(np.ndarray):
             self[row_index] = self[row_index] / pivot
 
     def _get_state(self):
-        ...
+        row, column = 0, 0
+        while row < (self.shape[0] - 1) and column < self.shape[1]:
+            if all(x == 0 for x in self[:, column][row+1:]):
+                row += 1
+                column += 1
+            else:
+                return NON_SIMPLIFIED
+
+        return UPPER_TRIANGULAR
 
     def reduce_echelon(self):
         """
         Transforms the matrix from upper triangular form to reduced echelon form
         :raise: NoSolutionException: if the matrix has no solution
         """
-        if self._get_state() != 'upper_triangle':
+        if self._get_state() != UPPER_TRIANGULAR:
             self.partial_pivot()
-            self.reduce_echelon()
 
         self._check_valid_solution()
         self._simplify_echelon()
@@ -92,8 +103,6 @@ class AugmentedMatrix(np.ndarray):
                     pivot = self[pivot_coord[0]][pivot_coord[1]]
                     factor = -1 * leading / pivot
                     self[i] = factor * self[pivot_coord[0]] + self[i]
-
-        self._state = 'reduce_echelon'
 
     def solve(self):
         """
